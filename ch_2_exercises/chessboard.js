dc6c4f1cb418f35b2831ec3b63675a875e55094f
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-15 14:40:14 -0700

    set up chessboard

---
+++ ch_2_exercises/chessboard.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines.
+ * At each position of the grid there is eather a space or a #
+ * characters should form a chessboard like this:
+ *  # # # #
+ * # # # #
+ *  # # # #
+ * # # # #
+ *  # # # #
+ * # # # #
+ *  # # # #
+ * # # # #
+ */
+
+const chessboard = () => {}
