ad0d21ead03825d4c833f760df8a5aac8aed400a
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-15 13:50:30 -0700

    loop prints 1 to 100

---
+++ ch_2_exercises/fizzBuzz.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: Write a program that prints all numbers from 1 to 100 with a few caveats:
+ * 1. numbers divisible by 3 print FIZZ
+ * 2. number divisible by 5 print BUZZ
+ * 3. numbers divisible by both 3 and 5 print FIZZBUZZ
+ *
+ */
+
+const fizzBuzz = () => {
+	for (let i = 1; i <= 100; i++) {
+		console.log(i)
+	}
+}
+
+fizzBuzz()
