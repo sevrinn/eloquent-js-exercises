2c87b6d48a3ce92f682176bad7e585a38759adc8
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-15 14:27:02 -0700

    commented my solution

--- ch_2_exercises/fizzBuzz.js
+++ ch_2_exercises/fizzBuzz.js
@@ -21,3 +21,14 @@ const fizzBuzz = () => {
 }
 
 fizzBuzz()
+
+/**
+ * my first solution is above:
+ * line 10: we create a loop that is initialized at 1, it will run until i is greater than 100, and value of i will increment by one after every cycle
+ * line 11: we use an AND logical operator inside of our if expression ensuring the number is divisible by both 3 and 5, printing FIZZBUZZ on line 12 if true
+ * line 13: if i is divisible by 5, it prints BUZZ on line 14
+ * line 15: if i is divisible by 3, it prints FIZZ on line 16
+ * line 19: in our ELSE block we just log the current value of i
+ *
+ * line 23: we invoke the fizzBuzz function
+ */
