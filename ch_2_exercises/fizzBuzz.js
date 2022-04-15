5baf656cd68e8ce8ec27e31e693068d32f2ef03d
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-15 14:03:43 -0700

    statements print fizz, buzz, and fizzbuzz conditionally instead of number

--- ch_2_exercises/fizzBuzz.js
+++ ch_2_exercises/fizzBuzz.js
@@ -8,7 +8,15 @@
 
 const fizzBuzz = () => {
 	for (let i = 1; i <= 100; i++) {
-		console.log(i)
+		if (i % 3 == 0 && i % 5 == 0) {
+			console.log('FIZZBUZZ')
+		} else if (i % 5 == 0) {
+			console.log('BUZZ')
+		} else if (i % 3 == 0) {
+			console.log('FIZZ')
+		} else {
+			console.log(i)
+		}
 	}
 }
 
