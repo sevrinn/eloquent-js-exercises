f49f54d2fc4124cc21ac6bfc41a6b0c6178f3161
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-15 13:40:27 -0700

    added comments

--- ch_2_exercises/looping_a_triangle.js
+++ ch_2_exercises/looping_a_triangle.js
@@ -15,13 +15,13 @@ for (let i = '#'; i.length <= 7; i += '#') {
 }
 
 //line 13: the initial expression is setting i as the #
-//the condition exression states that as long as the length of
-//the string i is less than or equal to 7 keep looping
+//the condition exression states that as long as the length of the string i is less than or equal to 7 keep looping
 // the increment expression will concatenate another # onto string i at the end of every cycle
-
+//line 14: inside our code block, we console the value of i (which is having another hash added to it every cycle)
 //this will happen until the condition expression is no longer true we will then jump out of the loop
 
 //the above is a rather elegant approach but kinda hard to read if you are an absolute beginner
+// the approach below was my original solution to this problem
 
 let str = '#'
 for (let i = 1; i <= 7; i++) {
