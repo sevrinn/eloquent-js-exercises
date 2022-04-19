0247a7e61366fe5676d3a8746bcf5741a4bd03bb
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-19 13:50:09 -0700

    reverseArray logs given array in reverse order

---
+++ ch_4_exercises/reversingArray.js
@@ -0,0 +1,9 @@
+/**
+ * Problem 1: Write  a function reverseArray that takes an array as an argument and produces a new array with the same elements in inverse order;
+ */
+const reverseArray = (arr) => {}
+
+/**
+ * Problem 2. Write a function reverseArrayInPlace, modifying the given array by reversing its elements;
+ */
+const reverseArrayInPlace = (arr) => {}
