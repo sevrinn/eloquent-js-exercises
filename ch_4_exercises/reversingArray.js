af5febb5e8edce3efa5f9494ac54160fe3778f84
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-19 13:52:17 -0700

    reversedArray returns new array with reversed values

--- ch_4_exercises/reversingArray.js
+++ ch_4_exercises/reversingArray.js
@@ -1,9 +1,18 @@
 /**
  * Problem 1: Write  a function reverseArray that takes an array as an argument and produces a new array with the same elements in inverse order;
  */
-const reverseArray = (arr) => {}
+const reverseArray = (arr) => {
+	let reversedArr = []
+	for (let i = arr.length - 1; i >= 0; i--) {
+		reversedArr.push(arr[i])
+	}
+	return reversedArr
+}
 
 /**
  * Problem 2. Write a function reverseArrayInPlace, modifying the given array by reversing its elements;
  */
-const reverseArrayInPlace = (arr) => {}
+// const reverseArrayInPlace = (arr) => {}
+
+let arrayValue = ['A', 'B', 'C']
+console.log(reverseArray(arrayValue))
