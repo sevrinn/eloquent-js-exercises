ea3e5cdff2bad275b99466e12d9198deba051e92
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-18 17:11:13 -0700

    commented for clarity

--- ch_3_exercises/minimum.js
+++ ch_3_exercises/minimum.js
@@ -13,5 +13,11 @@ const min = (a, b) => {
 console.log(min(33, 135))
 
 /**
+ * line 5: min function takes two values
+ * line 6: if a is less than b
+ * line 7: return a
+ * line 8: othewise
+ * line 9: return b (because it must be smaller than a)
  *
+ * line 13: calls min function with two arguments which are logged to the console
  */
