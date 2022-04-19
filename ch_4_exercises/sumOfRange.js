1c02a3bc56116bfc353c593e682575b5fb69a391
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-19 12:54:32 -0700

    range prints numbers from start to end numbers given

--- ch_4_exercises/sumOfRange.js
+++ ch_4_exercises/sumOfRange.js
@@ -3,4 +3,11 @@
  * 1. Write a range function that takes two arguments, start and end. It should return an array containing all the numbers from start up to and incuding end.
  */
 
-const range = (start, end) => {}
+const range = (start, end) => {
+	let myRange = []
+	for (let i = start; i <= end; i++) {
+		console.log(i)
+	}
+}
+
+range(1, 10)
