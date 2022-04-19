4e60bfdd30a4e4c32a34b378a3da607aeb5fa76a
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-19 13:28:19 -0700

    range now takes optional step input and accounts for negatives and 0

--- ch_4_exercises/sumOfRange.js
+++ ch_4_exercises/sumOfRange.js
@@ -3,11 +3,22 @@
  * 1. Write a range function that takes two arguments, start and end. It should return an array containing all the numbers from start up to and incuding end.
  */
 
-const range = (start, end) => {
+const range = (start, end, step = 1) => {
 	let myRange = []
-	for (let i = start; i <= end; i++) {
-		myRange.push(i)
+	if (step === 0) {
+		return 'Step cannot be zero'
+	} else if (step < 0) {
+		for (let i = start; i >= end; i += step) {
+			console.log('counting down', i)
+			myRange.push(i)
+		}
+	} else {
+		for (let i = start; i <= end; i += step) {
+			console.log('counting up', i)
+			myRange.push(i)
+		}
 	}
+
 	return myRange
 }
 
@@ -17,6 +28,9 @@ const range = (start, end) => {
  */
 
 const sum = (numArr) => {
+	if (numArr === 'Step cannot be zero') {
+		return 'Need new step input'
+	}
 	arrSum = 0
 	for (let num of numArr) {
 		arrSum += num
@@ -24,4 +38,13 @@ const sum = (numArr) => {
 	return arrSum
 }
 
-console.log(sum(range(1, 10)))
+console.log(sum(range(5, 2, -1)))
+console.log(sum(range(1, 10, 2)))
+console.log(sum(range(1, 10, 0)))
+
+/**
+ * BONUS ASSIGNMENT:
+ * Modify range function to take optional third argument that indicates the "step" value use when building the array.
+ * If no step is given, they will go up by one by default
+ *
+ */
