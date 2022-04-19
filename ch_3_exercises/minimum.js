0e9021ad2ef2210e130e319a2db514f0fa215d16
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-18 17:08:32 -0700

    minimum function written

---
+++ ch_3_exercises/minimum.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Wreite a function min that takes two arguments and returns their minimum.
+ */
+
+const min = (a, b) => {
+	if (a < b) {
+		return a
+	} else {
+		return b
+	}
+}
+
+console.log(min(33, 135))
+
+/**
+ *
+ */
