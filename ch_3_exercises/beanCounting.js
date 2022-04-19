4e6404f64430ab2d9e56f38273a3b76bafb2b490
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-18 17:48:55 -0700

    initial setup

---
+++ ch_3_exercises/beanCounting.js
@@ -0,0 +1,10 @@
+/**
+ * Problem: Wrtie a function countBs that takes a string as its only arg and returns a number that indicates how many uppercase B characters there are in a string
+ *
+ *          Next wrtie a function countChar that takes two args, str and targetChar. It returns a number indicating how many targetChar's there are in a string
+ *
+ */
+
+const countBs = (str) => {}
+
+const countChar = (str) => {}
