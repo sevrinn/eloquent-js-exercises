3ea36dc1c116ed9e1406a9326c70f9fd8ad990c7
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-19 12:50:02 -0700

    ch 4 folder and first exercise setup

---
+++ ch_4_exercises/sumOfRange.js
@@ -0,0 +1,6 @@
+/**
+ * Problems:
+ * 1. Write a range function that takes two arguments, start and end. It should return an array containing all the numbers from start up to and incuding end.
+ */
+
+const range = (start, end) => {}
