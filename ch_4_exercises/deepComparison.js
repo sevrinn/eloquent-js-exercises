34d4c8deca29563e1ffa1af9bf7dca754979514d
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-19 21:45:10 -0700

    deep comparison set up

---
+++ ch_4_exercises/deepComparison.js
@@ -0,0 +1,9 @@
+/** 
+ * Write function deepEqual that takes two values
+ * returns true only if values are the same
+ * or if they are objects with the same properties, where all values of properties are equal 
+ * when compared with a recursive call to deepEqual
+ */
+const deepEqual = (a, b) => {
+  
+}
