71605f658037de51a0daa554542591e59d7a8a62
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-22 15:40:40 -0700

    set up loop

---
+++ ch_5_exercises/yourOwnLoop.js
@@ -0,0 +1,10 @@
+/**
+ * Write a higher-order function loop that provides something like a for loop statement.
+ * It takes a value, a test, an update function, and a body function
+ * each iteration:
+ * 1. it runs the test function on current loop value and stops if that returns false
+ * 2. then it calls the body function, handing it the currentValue
+ * 3. calls the update function to create a new value and starts from beginning
+ */
+
+const loop = (value, test, updateFunction, bodyFunction) => {}
